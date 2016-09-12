4bcbf2e4470d2c24417de88b7a7db660ae780813
DESKTOP-T1045CH\son <sbh2ch@gmail.com>
2016-09-12 09:37:52 +0900

    no message

--- src/framework/WebUtil.java
+++ src/framework/WebUtil.java
@@ -9,12 +9,10 @@ public class WebUtil {
 	public static Object getParamToVO(Class<?> clz, HttpServletRequest req) throws Exception {
 		Object obj = clz.newInstance();
 		Method[] methods = clz.getMethods();
-		Map<String, String[]> params = req.getParameterMap();
 
-		for (String key : params.keySet()) {
+		for (String key : req.getParameterMap().keySet()) {
 			for (Method m : methods) {
 				if (m.getName().startsWith("set") && m.getName().substring(3).equalsIgnoreCase(key)) {
-					System.out.println(m.getName() + " / " + req.getParameter(key));
 					if (m.getParameterTypes()[0].getName().equals("int"))
 						m.invoke(obj, Integer.parseInt(req.getParameter(key)));
 					else
